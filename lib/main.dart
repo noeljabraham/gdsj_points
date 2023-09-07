b6b1b89b62768411e1fbbb144ed6042774d66a29
noelj <noeljabraham35@gmail.com>
2023-09-07 18:49:17 +0530

    after api print modification

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Your App Title',
+      title: 'GDSJ-Points',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
