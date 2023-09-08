64504be102d25b523284fd563c4b7c8d36d2a24e
Noel J Abraham <60876099+noeljabraham@users.noreply.github.com>
2023-09-08 19:41:19 +0530

    Update group.dart

--- lib/group.dart
+++ lib/group.dart
@@ -1,28 +1,116 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-
-
-
-
-class Group extends StatefulWidget {
-  const Group({super.key});
+class GroupScreen extends StatefulWidget {
+  const GroupScreen({super.key});
 
   @override
-  State<Group> createState() => _GroupState();
+  // ignore: library_private_types_in_public_api
+  _GroupScreenState createState() => _GroupScreenState();
 }
 
-class _GroupState extends State<Group> {
+class _GroupScreenState extends State<GroupScreen> {
+  // String secret = "onlyforgdscajce";
+  final body = {"secret": "onlyforgdscajce"};
+
+  Future fetchGroupScores() async {
+    try {
+      final response = await http.post(
+        Uri.parse('https://shy-fawn-fatigues.cyclic.app/group-scores'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(body),
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        return data;
+      } else {
+        return response.statusCode;
+      }
+    } catch (e) {
+      return e;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
-        child: Column(children: [
-          Padding(padding: EdgeInsets.only(top: 40),
-          child: 
-          Text("data"))
-        ]),
-      ),
-    );
+        resizeToAvoidBottomInset: false,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 36, left: 16),
+                child: Text(
+                  'GDSJ-Group Scores',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              FutureBuilder(
+                future: fetchGroupScores(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Container(
+                        alignment: Alignment.center,
+                        width: 400,
+                        height: 450,
+                        child: const CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Text('No data available');
+                  } else {
+                    final groupScores = snapshot.data;
+
+                    return Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Container(
+                          width: 400,
+                          height: 450,
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            children: [
+                              const Text(
+                                'Group Scores',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Expanded(
+                                child: ListView.builder(
+                                  itemCount: groupScores?.length,
+                                  itemBuilder: (context, index) {
+                                    final entry = groupScores?[index];
+                                    return ListTile(
+                                      title: Text(entry?['groupName'] ?? ''),
+                                      subtitle: Text(
+                                          'Score: ${entry?['score'] ?? ''}'),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
